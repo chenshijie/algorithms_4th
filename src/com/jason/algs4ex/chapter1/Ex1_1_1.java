acee0c51676018c18bf35922a7a505775f66d4e1
chenshijie <shijie.chen@gmail.com>
2022-12-06 17:10:02 +0800

    add question as comment before class definition

--- src/com/jason/algs4ex/chapter1/Ex1_1_1.java
+++ src/com/jason/algs4ex/chapter1/Ex1_1_1.java
@@ -2,6 +2,12 @@ package com.jason.algs4ex.chapter1;
 
 import edu.princeton.cs.algs4.StdOut;
 
+/*
+1.1.1 Give the value of each of the following expressions:
+a. ( 0 + 15 ) / 2
+b. 2.0e-6 * 100000000.1
+c. true && false || true && true
+ */
 public class Ex1_1_1 {
     public static void main(String[] args) {
         StdOut.println("1.1.1 答案：");
--- src/com/jason/algs4ex/chapter1/Ex1_1_2.java
+++ src/com/jason/algs4ex/chapter1/Ex1_1_2.java
@@ -1,7 +1,13 @@
 package com.jason.algs4ex.chapter1;
 
 import edu.princeton.cs.algs4.StdOut;
-
+/*
+1.1.2 Give the type and value of each of the following expressions:
+a. (1 + 2.236)/2
+b. 1 + 2 + 3 + 4.0
+c. 4.1 >= 4
+d. 1 + 2 + "3"
+ */
 public class Ex1_1_2 {
     public static void main(String[] args) {
         StdOut.println("1.1.2 答案：");
