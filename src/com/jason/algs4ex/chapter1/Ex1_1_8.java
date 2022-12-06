49602ae816ccf0e7edc4e3cd014ca8d8b3b411db
chenshijie <shijie.chen@gmail.com>
2022-12-06 19:40:01 +0800

    add Ex1_1_8

---
+++ src/com/jason/algs4ex/chapter1/Ex1_1_8.java
@@ -0,0 +1,19 @@
+package com.jason.algs4ex.chapter1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/*
+1.1.8 What do each of the following print?
+a. System.out.println('b');
+b. System.out.println('b' + 'c');
+c. System.out.println((char) ('a' + 4));
+ */
+public class Ex1_1_8 {
+    public static void main(String[] args) {
+        StdOut.println("Ex1_1_7");
+
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+    }
+}
