bca89b47099a9f23779ee13c0408ff27be220194
Jason <shijie.chen@gmail.com>
2022-12-07 21:48:25 +0800

    add Ex1_1_11

--- src/com/jason/algs4ex/chapter1/Ex1_1_30.java
+++ src/com/jason/algs4ex/chapter1/Ex1_1_30.java
@@ -3,12 +3,11 @@ package com.jason.algs4ex.chapter1;
 import edu.princeton.cs.algs4.StdIn;
 
 /*
-1.1.29 Equal keys. Add to BinarySearch a static method rank() that takes a key and a sorted array of int values
- (some of which may be equal) as arguments and returns the number of elements that are smaller than the key and a
- similar method count() that returns the number of elements equal to the key. Note : If i and j are the values returned
- byrank(key, a)andcount(key, a)respectively,thena[i..i+j-1]arethevaluesin the array that are equal to key.
+1.1.30 Array exercise. Write a code fragment that creates an N-by-N boolean array a[][] such that a[i][j] is true if i
+and j are relatively prime (have no common fac- tors), and false otherwise.
   */
 public class Ex1_1_30 {
+
     public static void main(String[] args) {
         System.out.println("Ex1_1_30");
         int N = StdIn.readInt();
