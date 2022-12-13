41b849114069550bfd9005f1f7934c4f4678b429
chenshijie <shijie.chen@gmail.com>
2022-12-13 23:14:26 +0800

    add Ex1_3_3

---
+++ src/com/jason/algs4ex/Stack.java
@@ -0,0 +1,81 @@
+package com.jason.algs4ex;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item> {
+    private Item[] stack;
+    private int number;
+
+    public Stack(int cap) {
+        this.stack = (Item[]) new Object[cap];
+        this.number = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.number == 0;
+    }
+
+    public boolean isFull() {
+        return stack.length == number;
+    }
+
+    public int size() {
+        return this.number;
+    }
+
+    public void push(Item item) {
+        if (isFull()) {
+            resize(2 * stack.length);
+        }
+        this.stack[number++] = item;
+    }
+
+    public Item pop() {
+        Item item = stack[--number];
+        stack[number] = null;
+        if (number > 0 && number == stack.length / 4) {
+            resize(stack.length / 2);
+        }
+        return item;
+    }
+
+    public void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        if (number >= 0) System.arraycopy(stack, 0, temp, 0, number);
+        stack = temp;
+    }
+
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(stack, number));
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    public class ReverseArrayIterator implements Iterator<Item> {
+        private int i = number;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return stack[--i];
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+}
