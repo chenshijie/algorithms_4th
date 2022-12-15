9175d5221a542024a1daf7695b02a169ea2ea2af
chenshijie <shijie.chen@gmail.com>
2022-12-15 13:10:55 +0800

    add Ex1_3_28

--- src/com/jason/algs4ex/chapter1/Ex1_3_28.java
+++ src/com/jason/algs4ex/chapter1/Ex1_3_28.java
@@ -5,9 +5,7 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /*
-1.3.27 Write a method max() that takes a reference to the first node in a linked list as
-argument and returns the value of the maximum key in the list. Assume that all keys are
-positive integers, and return 0 if the list is empty.
+1.3.28 Develop a recursive solution to the previous question.
  */
 public class Ex1_3_28 {
     public static void main(String[] args) {
