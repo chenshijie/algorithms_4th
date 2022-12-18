9c4b3299a4708ed776d8ee4b3cba693a2c47abe6
Jason <shijie.chen@gmail.com>
2022-12-18 19:34:44 +0800

    Add Ex1_3_49

--- src/com/jason/algs4ex/ch1_4/Ex1_4_3.java
+++ src/com/jason/algs4ex/ch1_4/Ex1_4_3.java
@@ -1,6 +1,6 @@
 package com.jason.algs4ex.ch1_4;
 
-import edu.princeton.cs.algs4.DoublingTest;
+import com.jason.algs4ex.DoublingTest;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -9,20 +9,21 @@ import edu.princeton.cs.algs4.StdOut;
 */
 public class Ex1_4_3 {
     public static void main(String[] args) {
-        int count = 10;
+        int count = 20;
+        int step = 200;
         double[] numbers = new double[count];
         double[] times = new double[count];
         numbers[0] = 0;
         times[0] = 0;
         for (int i = 1; i < count; i++) {
-            int N = i * 500;
+            int N = i * step;
             double time = DoublingTest.timeTrial(N);
             numbers[i] = N;
             times[i] = time;
-            StdOut.printf("%7d %5.3f\n", N, time);
+            StdOut.printf("%7d %5.5f\n", N, time);
         }
         double maxNumbers = numbers[count - 1];
-        double maxTime = times[count - 1];
+        double maxTime = max(times);
         StdDraw.setXscale(-maxNumbers * 0.05, maxNumbers * 1.05);
         StdDraw.setYscale(-maxTime * 0.05, maxTime * 1.05);
         StdDraw.line(0, 0, maxNumbers, 0);
@@ -30,5 +31,15 @@ public class Ex1_4_3 {
         for (int i = 1; i < count; i++) {
             StdDraw.line(numbers[i - 1], times[i - 1], numbers[i], times[i]);
         }
+    }
+
+    public static double max(double[] array) {
+        assert array.length > 0;
+        double max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max, array[i]);
+        }
+        return max;
+
     }
 }
