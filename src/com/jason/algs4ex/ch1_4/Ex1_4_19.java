8e8463a5b5a1b2ca90b162d54cc236835e0a0af3
Jason <shijie.chen@gmail.com>
2022-12-19 14:15:40 +0800

    Add Ex1_4_19

--- src/com/jason/algs4ex/ch1_4/Ex1_4_19.java
+++ src/com/jason/algs4ex/ch1_4/Ex1_4_19.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.StdRandom;
 import java.util.Arrays;
 
 /*
-.4.19 Local minimum of a matrix. Given an N-by-N array a[] of N 2 distinct integers,
+1.4.19 Local minimum of a matrix. Given an N-by-N array a[] of N 2 distinct integers,
 design an algorithm that runs in time proportional to N to find a local minimum:
 a pair of indices i and j such that a[i][j] < a[i+1][j], a[i][j] < a[i][j+1],
 a[i][j] < a[i-1][j], and a[i][j] < a[i][j-1]. The running time of your program
