3bf155371b7e9867b85d26bf08735927f055665d
Jason <shijie.chen@gmail.com>
2022-12-26 21:20:56 +0800

    fix bug

--- src/com/jason/RandomGrid.java
+++ src/com/jason/RandomGrid.java
@@ -2,6 +2,7 @@ package com.jason;
 
 import com.jason.algs4ex.Connection;
 import com.jason.algs4ex.RandomBag;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomGrid {
@@ -10,12 +11,13 @@ public class RandomGrid {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
+                StdOut.println();
                 if (StdRandom.bernoulli(0.5)) {
                     bag.add(new Connection(i * n + j, (i * n) + j + 1));
-                    bag.add(new Connection(i * n + j, ((i + 1) * n) + j));
+                    bag.add(new Connection(j * n + i, ((j + 1) * n) + i));
                 } else {
                     bag.add(new Connection((i * n) + j + 1, i * n + j));
-                    bag.add(new Connection(((i + 1) * n) + j, i * n + j));
+                    bag.add(new Connection(((j + 1) * n) + i, j * n + i));
                 }
             }
         }
