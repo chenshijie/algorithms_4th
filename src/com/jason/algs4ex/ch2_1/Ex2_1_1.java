16ffdfaa1322b0bf4c28ff5fcf59b3503f3f52ff
Jason <shijie.chen@gmail.com>
2022-12-28 13:14:21 +0800

    add Ex2_1_1

---
+++ src/com/jason/algs4ex/ch2_1/Ex2_1_1.java
@@ -0,0 +1,10 @@
+package com.jason.algs4ex.ch2_1;
+
+/*
+2.1.1 按照算法2.1所示轨迹的格式给出选择排序是如何将数组E A S Y Q U E S T I O N排序的。
+ */
+public class Ex2_1_1 {
+    public static void main(String[] args) {
+        System.out.println("see com.jason.algs4ex.Selection");
+    }
+}
