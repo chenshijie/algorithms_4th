24e31e658de733520602bf5ba12564d72152b27d
Jason <shijie.chen@gmail.com>
2022-12-29 09:00:00 +0800

    add Ex2_1_22

---
+++ src/com/jason/algs4ex/ch2_1/Ex2_1_22.java
@@ -0,0 +1,14 @@
+package com.jason.algs4ex.ch2_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/*
+2.1.22 交易排序测试用例。编写一个SortTransaction类，在静态方法main()中从标准输入读取一系列交易
+，将它们排序并在标准输出中打印结果（请见练习1.3.17）。
+*/
+public class Ex2_1_22 {
+
+    public static void main(String[] args) {
+        StdOut.println("See Ex2_1_21");
+    }
+}
