4d4ae1cdd614fbbf06c4e24f5e94b3437abb0073
Jason <shijie.chen@gmail.com>
2022-12-29 08:29:02 +0800

    add Ex2_1_18

---
+++ src/com/jason/algs4ex/ch2_1/Ex2_1_18.java
@@ -0,0 +1,11 @@
+package com.jason.algs4ex.ch2_1;
+
+/*
+2.1.18 可视轨迹。修改你为上一题给出的解答，为插入排序和选择排序生成和正文中类似的可视轨迹。
+提示：使用setYscale()函数是一个明智的选择。附加题：添加必要的代码，与正文中的图片一样用红色和灰色强调不同角色的元素。
+ */
+public class Ex2_1_18 {
+    public static void main(String[] args) {
+        System.out.println("see Ex2_1_17");
+    }
+}
