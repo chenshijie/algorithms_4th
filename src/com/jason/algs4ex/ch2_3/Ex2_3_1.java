e551f0b45b120f0a7170cf241f69a9d208b2dcfb
Jason <shijie.chen@gmail.com>
2023-01-02 15:34:04 +0800

    add Ex2_3_1

--- src/com/jason/algs4ex/ch2_3/Ex2_3_1.java
+++ src/com/jason/algs4ex/ch2_3/Ex2_3_1.java
@@ -23,9 +23,10 @@ public class Ex2_3_1 {
             StringBuilder sb = new StringBuilder();
             StringBuilder line = new StringBuilder();
             sb.append(ConsoleColor.color("lo", "%4s", ConsoleColor.COLOR_BLUE));
+            sb.append(ConsoleColor.color("i", "%4s", ConsoleColor.COLOR_BLUE));
             sb.append(ConsoleColor.color("j", "%4s", ConsoleColor.COLOR_BLUE));
             sb.append(ConsoleColor.color("hi", "%4s", ConsoleColor.COLOR_BLUE));
-            line.append("              ");
+            line.append("                  ");
             for (int i = 0; i < a.length; i++) {
                 sb.append(ConsoleColor.color(i + "", "%4s", ConsoleColor.COLOR_BLUE));
                 if (i == 0) {
@@ -39,54 +40,63 @@ public class Ex2_3_1 {
             StdOut.println(line);
         }
 
-        public static String getLeftCoordinate(int lo, int j, int hi) {
+        public static String getLeftCoordinate(int lo, int i, int j, int hi) {
+            if (i < 0 || j < 0 || lo < 0 || hi < 0) {
+                return "                ";
+            }
             StringBuilder sb = new StringBuilder();
 
             int color = ConsoleColor.COLOR_YELLOW;
             if (lo == hi) {
                 color = ConsoleColor.COLOR_GRAY;
             }
-            if (lo >= 0) {
-                sb.append(ConsoleColor.color(lo + "", "%4s", color));
-            } else {
-                sb.append("    ");
-            }
-            if (j >= 0) {
-                sb.append(ConsoleColor.color(j + "", "%4s", ConsoleColor.COLOR_RED, ConsoleColor.FONT_TYPE_BOLD));
-            } else {
-                sb.append("    ");
-            }
-            if (hi >= 0) {
-                sb.append(ConsoleColor.color(hi + "", "%4s", color));
-            } else {
-                sb.append("    ");
+            sb.append(ConsoleColor.color(lo + "", "%4s", ConsoleColor.COLOR_GREEN));
+            sb.append(ConsoleColor.color(i + "", "%4s", color));
+            sb.append(ConsoleColor.color(j + "", "%4s", color));
+            sb.append(ConsoleColor.color(hi + "", "%4s", color));
+
+            return sb.toString();
+        }
+
+        public static <T> String getFormatData(T[] a, int lo, int i, int j, int hi) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(getLeftCoordinate(lo, i, j, hi));
+
+            for (int n = 0; n < a.length; n++) {
+                if (n < lo || n > hi) {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_GRAY));
+                } else if (n == lo) {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_GREEN, ConsoleColor.FONT_TYPE_BOLD));
+                } else if (n == i) {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_BLUE));
+                } else if (n == j) {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_BLUE));
+                } else {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_YELLOW));
+                }
             }
             return sb.toString();
         }
 
-        public static <T> String getFormatData(T[] a, int lo, int j, int hi) {
+        public static <T> String getExchangeFormatData(T[] a, int i, int j) {
             StringBuilder sb = new StringBuilder();
-            sb.append(getLeftCoordinate(lo, j, hi));
+            sb.append(getLeftCoordinate(-1, -1, -1, -1));
 
-            for (int i = 0; i < a.length; i++) {
-                if (i < lo || i > hi) {
-                    sb.append(ConsoleColor.color(a[i] + "", "%4s", ConsoleColor.COLOR_GRAY));
-                } else if (i == j) {
-                    sb.append(ConsoleColor.color(a[i] + "", "%4s", ConsoleColor.COLOR_RED, ConsoleColor.FONT_TYPE_BOLD));
+            for (int n = 0; n < a.length; n++) {
+                if (n == i || n == j) {
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_RED));
                 } else {
-                    sb.append(ConsoleColor.color(a[i] + "", "%4s", ConsoleColor.COLOR_YELLOW));
+                    sb.append(ConsoleColor.color(a[n] + "", "%4s", ConsoleColor.COLOR_GRAY));
                 }
             }
-
             return sb.toString();
-
         }
 
         public static <T extends Comparable<T>> void sort(T[] a) {
             printHeader(a);
-            StdOut.println(getFormatData(a, -1, -1, -1));
+            StdOut.println(getFormatData(a, -1, -1, -1, -1));
             StdRandom.shuffle(a);
-            StdOut.println(getFormatData(a, -1, -1, -1));
+            StdOut.println(getFormatData(a, -1, -1, -1, -1));
 
             sort(a, 0, a.length - 1);
         }
@@ -96,7 +106,7 @@ public class Ex2_3_1 {
                 return;
             }
             int j = partition(a, lo, hi);
-            StdOut.println(getFormatData(a, lo, j, hi));
+
             sort(a, lo, j - 1);
             sort(a, j + 1, hi);
         }
@@ -114,23 +124,32 @@ public class Ex2_3_1 {
         public static <T extends Comparable<T>> int partition(T[] a, int lo, int hi) {
             int i = lo, j = hi + 1;
             T v = a[lo];
+            StdOut.println(getFormatData(a, lo, i, j, hi));
             while (true) {
                 while (less(a[++i], v)) {
+                    StdOut.println(getFormatData(a, lo, i, j, hi));
                     if (i == hi) {
                         break;
                     }
                 }
+                StdOut.println(getFormatData(a, lo, i, j, hi));
                 while (less(v, a[--j])) {
+                    StdOut.println(getFormatData(a, lo, i, j, hi));
                     if (j == lo) {
                         break;
                     }
                 }
+                StdOut.println(getFormatData(a, lo, i, j, hi));
                 if (i >= j) {
                     break;
                 }
+                StdOut.println(getExchangeFormatData(a, i, j));
                 exchange(a, i, j);
+                StdOut.println(getExchangeFormatData(a, i, j));
             }
+            StdOut.println(getExchangeFormatData(a, lo, j));
             exchange(a, lo, j);
+            StdOut.println(getExchangeFormatData(a, lo, j));
             return j;
         }
     }
