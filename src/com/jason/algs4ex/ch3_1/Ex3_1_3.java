09c6fb6b0d7f66d52bc302f638cf83b6c975665b
Jason <shijie.chen@gmail.com>
2023-03-28 20:19:58 +0800

    add Ex3_1_3

---
+++ src/com/jason/algs4ex/ch3_1/Ex3_1_3.java
@@ -0,0 +1,106 @@
+package com.jason.algs4ex.ch3_1;
+
+/*
+3.1.3 开发一个符号表的实现OrderedSequentialSearchST，使用有序链表来实现我们的有序符号表API。
+ */
+
+import com.jason.algs4ex.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+class OrderedSequentialSearchST<Key extends Comparable<Key>, Value> {
+    private int n;
+    private Node first;
+
+    public OrderedSequentialSearchST() {
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        n++;
+    }
+
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        first = delete(first, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
+    }
+
+    private class Node {
+        private final Key key;
+        private Value val;
+        private Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+public class Ex3_1_3 {
+    public static void main(String[] args) {
+        //S E A R C H E X A M P L E
+        OrderedSequentialSearchST<String, Integer> st;
+        st = new OrderedSequentialSearchST<>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        for (String s : st.keys()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+    }
+}
