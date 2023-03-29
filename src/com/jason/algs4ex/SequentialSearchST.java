18f81627724c3ab4737534d074541d1835bcd092
Jason <shijie.chen@gmail.com>
2023-03-29 22:24:02 +0800

    add SequentialSearchST

---
+++ src/com/jason/algs4ex/SequentialSearchST.java
@@ -0,0 +1,85 @@
+package com.jason.algs4ex;
+
+import edu.princeton.cs.algs4.Queue;
+
+public class SequentialSearchST<Key, Value> {
+    private int length;
+    private Node first;
+
+    public SequentialSearchST() {
+    }
+
+    public int size() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        first = delete(first, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            length--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        length++;
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
+    }
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
